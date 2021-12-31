7e9a9c718a749b713f364e45782d28286ec2e10e
Your Name <ajiqbrainx>
2021-12-31 12:51:31 +0530

    add

---
+++ src/main/scala/Dec31/Main1.scala
@@ -0,0 +1,27 @@
+package Dec31
+
+
+  object Main1 extends App{
+
+    val a:PartialFunction[Int,Int]={
+
+      case x if x %2==0=>10*x
+
+    }
+
+
+
+    val r:PartialFunction[Int,Int]={
+
+      case x if x !=0=>10*x
+
+    }
+
+
+
+    val res=a andThen  r
+
+    println(res(10))
+
+
+}
