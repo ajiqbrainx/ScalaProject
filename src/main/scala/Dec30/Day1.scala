6c032f46acd14ade692fe083a4d3a346b0a5804a
Your Name <ajiqbrainx>
2022-01-03 13:03:38 +0530

    add

---
+++ src/main/scala/Dec30/Day1.scala
@@ -0,0 +1,26 @@
+package Dec30
+
+sealed trait Animal1
+
+class Dog extends Animal1
+class Cat extends Animal1
+class Lion extends Animal1
+class Tiger extends Animal1
+class Cheetah extends Animal1
+
+
+object Day1 extends App{
+
+  def wildAnimal(animal: Animal1)=animal match
+
+  {
+    case _:Cat =>false
+    case _:Dog => false
+    case i:Cheetah => true
+    case i:Lion => true
+    case _=>"It is not animal"
+  }
+  println(wildAnimal(new Cat))
+  println(wildAnimal(new Dog))
+  println(wildAnimal(new Lion))
+}
