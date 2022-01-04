a52840727a95225bbfe08ad459ebb9eeb5789dc7
Your Name <ajiqbrainx>
2022-01-04 12:52:12 +0530

    add

--- src/main/scala/Jan03/Implicit.scala
+++ src/main/scala/Jan03/Implicit.scala
@@ -2,10 +2,18 @@ package Jan03
 
 object Implicit extends App {
 
+  val partialFunction:PartialFunction[Int,String]={
+    case y if (y%2)==0 => s"$y"
+    case _=>"Y"
+  }
+  println(partialFunction(4))
+
+
   implicit val integer1 = 67
   implicit val double1 = 67.4
   implicit val bool = true
   implicit val string="Ajith"
+
 
   def implicit1(a: Int)(implicit b: Int): Int = a + b
 
@@ -23,5 +31,7 @@ object Implicit extends App {
   val bb1 = implicit2(12)
   println(bb)
 println(bb1)
+
+
 
 }
